58e3218b8e00b292247ceb378334713ea4bcaa61
ciprian-macovei <macoveiciprian91@gmail.com>
2020-03-30 20:58:17 +0300

    fix

---
+++ public/build/precache-manifest.526c669a03d99280cb940aec48a965b3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70913b3419e264b973bacccbb0b1884c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58ba10cfa8b3cdc2e8f6",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "76dff5b65ba3cc5f6e89",
+    "url": "/static/css/main.5a6b5474.chunk.css"
+  },
+  {
+    "revision": "58ba10cfa8b3cdc2e8f6",
+    "url": "/static/js/2.16d38a18.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.16d38a18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76dff5b65ba3cc5f6e89",
+    "url": "/static/js/main.b0dc9295.chunk.js"
+  },
+  {
+    "revision": "f8e92c0beecd66a677df",
+    "url": "/static/js/runtime-main.ad63209e.js"
+  },
+  {
+    "revision": "de9f1a117817128e78b9e01133d05f83",
+    "url": "/static/media/earth.de9f1a11.jpg"
+  },
+  {
+    "revision": "ccf2796466a0a3a7cf3c46f8969eb338",
+    "url": "/static/media/slide1.ccf27964.jpeg"
+  },
+  {
+    "revision": "dfc585755abac59921854815398d5f41",
+    "url": "/static/media/slide2.dfc58575.jpg"
+  }
+]);
